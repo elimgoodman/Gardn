47cdda2364724f0fc07c12273d85b45f06c634d3
Eli Goodman <eli.m.goodman@gmail.com>
2012-06-04 08:55:34 -0400

    discussions

--- index.py
+++ index.py
@@ -16,6 +16,19 @@ class MessageRaw(mongo.Document):
     def getBody(self):
         return email.message_from_string(str(self.message_raw))
 
+    def getBodyField(self, field):
+        body = self.getBody()
+        return body[field]
+
+#class Messsage(mongo.EmbeddedDocument):
+    #gm_msg_id = mongo.IntField(required=True)
+    #content = mongo.StringField()
+
+class Discussion(mongo.Document):
+    gm_thread_id = mongo.IntField(unique=True)
+    subject = mongo.StringField()
+    messages = mongo.SortedListField(mongo.ReferenceField(MessageRaw))
+
 app = Flask(__name__)
 
 @app.route('/refresh')
@@ -24,19 +37,33 @@ def refresh():
 
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
     mail.login(EmailCredentials.ADDRESS, EmailCredentials.PASSWORD)
-    mail.select("inbox")
-    
-    #all msg uids
-    result, data = mail.uid('search', None, "ALL")
-    uids = data[0].split()
+    boxes = ["inbox", "[Gmail]/Sent Mail"]
 
-    for uid in uids:
-        result, data = mail.uid('fetch', uid, '(RFC822 X-GM-THRID X-GM-MSGID)')
-        msg_data = data[0]
-        (id_data, message_raw) = msg_data
-        id_parsed = re.search(r'X-GM-THRID (?P<gm_thread_id>\d+) X-GM-MSGID (?P<gm_msg_id>\d+)', id_data).groupdict()
-        params = {'gm_thread_id': id_parsed['gm_thread_id'], 'message_raw': message_raw}
-        MessageRaw.objects.get_or_create(gm_msg_id=id_parsed['gm_msg_id'], defaults=params)
+    for box in boxes:
+        mail.select(box)
+        
+        #all msg uids
+        result, data = mail.uid('search', None, "ALL")
+        uids = data[0].split()
+
+        for uid in uids:
+            result, data = mail.uid('fetch', uid, '(RFC822 X-GM-THRID X-GM-MSGID)')
+            msg_data = data[0]
+            (id_data, message_raw) = msg_data
+            id_parsed = re.search(r'X-GM-THRID (?P<gm_thread_id>\d+) X-GM-MSGID (?P<gm_msg_id>\d+)', id_data).groupdict()
+            params = {'gm_thread_id': id_parsed['gm_thread_id'], 'message_raw': message_raw}
+            (msg_raw, created) = MessageRaw.objects.get_or_create(gm_msg_id=id_parsed['gm_msg_id'], defaults=params)
+
+            #body = msg_raw.getBody()
+            #parts = body.get_payload()
+            #FIXME: arbitrary
+            #msg = Message(gm_msg_id=id_parsed['gm_msg_id'], content=part[0].get_payload())
+
+            #FIXME: how to set subject to newest?
+            disc_params = {'messages': [], 'subject': msg_raw.getBodyField('Subject')}
+            (discussion, created) = Discussion.objects.get_or_create(gm_thread_id=id_parsed['gm_thread_id'], defaults=disc_params)
+            discussion.messages.append(msg_raw)
+            discussion.save()
 
     return redirect("/")
 
@@ -44,22 +71,16 @@ def refresh():
 def index():
     connect()
     
-    msgs = MessageRaw.objects()
-    emails = []
-    for msg in msgs:
-        emails.append({
-            'body': msg.getBody(),
-            'msg': msg
-        })
-    return render_template('index.jinja', emails=emails)
+    discussions = Discussion.objects()
+    return render_template('index.jinja', discussions=discussions)
 
 
 @app.route('/discussion/<gm_thread_id>')
 def discussion(gm_thread_id):
     connect()
     
-    msgs = MessageRaw.objects(gm_thread_id=gm_thread_id)
-    return render_template('discussion.jinja', msgs=msgs)
+    discussion = Discussion.objects.get(gm_thread_id=gm_thread_id)
+    return render_template('discussion.jinja', discussion=discussion)
 
 if __name__ == '__main__':
     app.run(debug=True)
